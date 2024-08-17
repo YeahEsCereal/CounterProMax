ba32a6c87a382a67bf2369ff838c16514b95390f
YeahEsCereal <153660696+YeahEsCereal@users.noreply.github.com>
2024-08-17 09:18:52 +0100

    Add files via upload
    All the code!

---
+++ script.js
@@ -0,0 +1,40 @@
+value = 0
+max = 100
+
+function add() {
+    value += 1
+    if (max < 0) {max = 0}
+    if (value > max) {console.log('asd');value = max}
+    document.querySelector('.inner').style.height = `${(document.querySelector('.outer').offsetHeight) - 20}px`
+    document.querySelector('.inner').style.width = `${value/max * document.querySelector('.outer').offsetWidth - 20}px`
+    document.querySelector('.inner').innerHTML = value.toString()
+}
+
+function subtract() {
+    value -= 1
+    if (value < 0) {value = 0}
+    if (max < 0) {max = 0}
+    if (value > max) {value = max}
+    document.querySelector('.inner').style.height = `${(document.querySelector('.outer').offsetHeight) - 20}px`
+    document.querySelector('.inner').style.width = `${value/max * document.querySelector('.outer').offsetWidth - 20}px`
+    document.querySelector('.inner').innerHTML = value.toString()
+}
+
+function reset() {
+    value = 0
+    if (max < 0) {max = 0}
+    if (value > max) {value = max}
+    document.querySelector('.inner').style.height = `${(document.querySelector('.outer').offsetHeight) - 20}px`
+    document.querySelector('.inner').style.width = `${value/max * document.querySelector('.outer').offsetWidth}px`
+    document.querySelector('.inner').innerHTML = value.toString()
+}
+
+document.querySelector('input').addEventListener('input', () => {
+    max = document.querySelector('input').value
+    if (max < 0) {max = 0}
+    if (max < value) {max = value}
+    document.querySelector('.inner').style.height = `${(document.querySelector('.outer').offsetHeight) - 20}px`
+    document.querySelector('.inner').style.width = `${value/max * document.querySelector('.outer').offsetWidth - 20}px`
+    document.querySelector('input').value = max
+    document.querySelector('.inner').innerHTML = value.toString()
+})
